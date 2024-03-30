03a1afae8605e3b5d610e1d7df74d24d7c2ebd6a
Rish-XO <rishal.mhd009@gmail.com>
2024-03-30 17:33:21 +0530

    fixed web3 contructor issue

--- controllers/web3Controller.js
+++ controllers/web3Controller.js
@@ -1,28 +1,30 @@
-const Web3 = require("web3");
+const { Web3 } = require('web3');
 
-// alchemy provider
-const web3 = new Web3(
-  "https://eth-sepolia.g.alchemy.com/v2/fWr3m1Bq4Mqxz0n-WoE86aq24VsXTsrq"
-);
+// Alchemy provider URL
+const providerUrl = 'https://eth-sepolia.g.alchemy.com/v2/fWr3m1Bq4Mqxz0n-WoE86aq24VsXTsrq';
+
+// Create a new Web3 instance
+const web3 = new Web3(providerUrl);
 
 exports.fetchBalance = async (req, res) => {
   const { address } = req.params;
 
   try {
     // Validate the Ethereum address
-    if (!web3.utils.isAddress(address)) {
-      return res.status(400).json({ error: "Invalid Ethereum address" });
+    const isValidAddress = Web3.utils.isAddress(address);
+    if (!isValidAddress) {
+      return res.status(400).json({ error: 'Invalid Ethereum address' });
     }
 
     // Get the account balance in Wei
     const balance = await web3.eth.getBalance(address);
 
     // Convert the balance from Wei to Ether
-    const balanceInEther = web3.utils.fromWei(balance, "ether");
+    const balanceInEther = Web3.utils.fromWei(balance, 'ether');
 
     res.json({ balance: balanceInEther });
   } catch (error) {
-    console.error("Error fetching account balance:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+    console.error('Error fetching account balance:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
