07d9c73af8ce2cf8d9f10c41aabc3c86618f6ad8
Rish-XO <rishal.mhd009@gmail.com>
2024-03-30 15:40:17 +0530

    moved the fecth data to contolller file

--- controllers/dataConroller.js
+++ controllers/dataConroller.js
@@ -1,1 +1,42 @@
 const axios = require('axios'); 
+
+exports.fetchData = async (req, res) => {
+    try {
+        // Fetch data from the public API
+        const response = await axios.get("https://api.publicapis.org/entries");
+        const { entries } = response.data;
+    
+        // Apply filtering options
+        let filteredData = entries;
+    
+        // Filter by category if specified
+        const category = req.query.category;
+        if (category) {
+          //edge case of invalid category
+          const categoryExists = entries.some(
+            (api) => api.Category.toLowerCase() === category.toLowerCase()
+          );
+          if (!categoryExists) {
+            return res.status(404).json({ message: "Category not found" });
+          }
+          filteredData = filteredData.filter(
+            (api) => api.Category.toLowerCase() === category.toLowerCase()
+          );
+        }
+    
+        // Limit results if specified
+        const limit = req.query.limit ? parseInt(req.query.limit) : entries.length;
+    
+        //edge case for invalid limit
+        if (isNaN(limit) || limit < 0) {
+          return res.status(400).json({ message: "Invalid limit parameter" });
+        }
+    
+        filteredData = filteredData.slice(0, limit);
+    
+        res.json(filteredData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+  };
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcryptjs");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const { register, login, logout } = require("./controllers/authController");
+const { fetchData } = require("./controllers/dataConroller");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -22,6 +23,10 @@ app.post("/login", login);
 
 // Endpoint for user logout
 app.post("/logout", logout);
+
+//*** TASK 2 */
+// API endpoint to fetch data
+app.get("/data-api", fetchData);
 
 //*** TASK 4 */
 // Middleware to verify JWT token
@@ -50,48 +55,6 @@ app.get("/protected", verifyToken, (req, res) => {
   });
 });
 
-//*** TASK 2 */
-// API endpoint to fetch data
-app.get("/data-api", async (req, res) => {
-  try {
-    // Fetch data from the public API
-    const response = await axios.get("https://api.publicapis.org/entries");
-    const { entries } = response.data;
-
-    // Apply filtering options
-    let filteredData = entries;
-
-    // Filter by category if specified
-    const category = req.query.category;
-    if (category) {
-      //edge case of invalid category
-      const categoryExists = entries.some(
-        (api) => api.Category.toLowerCase() === category.toLowerCase()
-      );
-      if (!categoryExists) {
-        return res.status(404).json({ message: "Category not found" });
-      }
-      filteredData = filteredData.filter(
-        (api) => api.Category.toLowerCase() === category.toLowerCase()
-      );
-    }
-
-    // Limit results if specified
-    const limit = req.query.limit ? parseInt(req.query.limit) : entries.length;
-
-    //edge case for invalid limit
-    if (isNaN(limit) || limit < 0) {
-      return res.status(400).json({ message: "Invalid limit parameter" });
-    }
-
-    filteredData = filteredData.slice(0, limit);
-
-    res.json(filteredData);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
